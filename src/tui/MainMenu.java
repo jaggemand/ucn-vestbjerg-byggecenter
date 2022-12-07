03c5a0d74c53ce71b283edbc0fcd69160512ca83
MikkelHentze <115947117+MikkelHentze@users.noreply.github.com>
2022-12-07 09:16:48 +0100

    Update MainMenu.java
    Mikkel was here again

--- src/tui/MainMenu.java
+++ src/tui/MainMenu.java
@@ -3,6 +3,8 @@ package tui;
 public class MainMenu {
 
 	public static void main(String[] args) {
+		//Mikkel was here
+		
 		// TODO Auto-generated method stub
 		System.out.println("Hello World");
 	}
