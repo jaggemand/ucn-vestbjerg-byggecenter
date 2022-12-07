036db6e935c312afee1089a7a4c4f2c4c3181176
NikolaiCS <113992103+NikolaiCS@users.noreply.github.com>
2022-12-07 08:57:35 +0100

    added comment to main menu code

--- src/tui/MainMenu.java
+++ src/tui/MainMenu.java
@@ -2,6 +2,7 @@ package tui;
 
 public class MainMenu {
 
+	//this is the main method
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello World");
