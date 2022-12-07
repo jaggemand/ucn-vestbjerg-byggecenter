dd6d9b425f12c3b8fe0aae7112b898c9af6a913a
esserdk88 <rasmus.lyngberg@gmail.com>
2022-12-07 09:22:52 +0100

    Update MainMenu.java

--- src/tui/MainMenu.java
+++ src/tui/MainMenu.java
@@ -2,12 +2,19 @@ package tui;
 
 public class MainMenu {
 
+<<<<<<< Updated upstream
 	//this is the main method
+=======
+	//alksfbaklfg
+	//sdaqwwsasfga
+>>>>>>> Stashed changes
 	public static void main(String[] args) {
 		//Mikkel was here
 		
 		// TODO Auto-generated method stub
+		System.out.println("Hey");
 		System.out.println("Hello World");
+		System.out.println("WOOT");
 	}
 
 }
