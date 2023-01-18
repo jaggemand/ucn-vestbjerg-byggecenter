743badf3a9077c5c5196dfa4e51266ee37dd56c4
MikkelHentze <115947117+MikkelHentze@users.noreply.github.com>
2023-01-18 11:16:54 +0100

    Class: customerController added

---
+++ src/controller/CustomerController.java
@@ -0,0 +1,38 @@
+package controller;
+
+import java.util.ArrayList;
+
+public class CustomerController {
+	private Customer customer;
+	
+	
+	public boolean addCustomer() {
+		
+		
+	}
+	
+	public boolean removeCustomer(String customerID) {
+		
+		
+	}
+	
+	public Customer setCustomer() {
+		
+		
+	}
+	
+	public Customer getCustomer() {
+		
+		
+	}
+	
+	public ArrayList<Customer> findCustomerByInformation(){
+		
+		
+	}
+	
+	public ArrayList<Customer> getAllCustomers(){
+		
+		
+	}
+}
